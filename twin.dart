958b601b1aec937b268f125e1f1345d2e9af4945
Arun-pushpan <93389122+Arun-pushpan@users.noreply.github.com>
2022-10-11 16:22:29 +0530

    Update twin.dart

--- twin.dart
+++ twin.dart
@@ -1,107 +1,30 @@
    
+   Container(
+                      padding: EdgeInsets.all(10),
+                      child: Center(
+                        child: RichText(
+                          text: TextSpan(
+                              text: 'Need an account?',
+                              style: TextStyle(
+                                  color: Colors.red, fontSize: 20),
+                              children: <TextSpan>[
+                                TextSpan(text: ' Sign up',
+                                    style: TextStyle(
+                                        color: Colors.blueAccent, fontSize: 20),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                  Navigator.pushNamed(context, route.registerPage);
+                                        // navigate to desired screen
+                                      }
+                                )
+                              ]
+                          ),
+                        ),
+                      )
+                  )
   
   
   
   
-  
-  
-Feel free to use the code in your projects but do not forget to give me the credits adding my app (Flutter Animation Gallery) where you are gonna use it.
-
-------------------------------------------
-
- 
-
-
-import 'dart:ui';
-import 'package:flutter/material.dart';
-import 'dart:math';
-
-class MyCustomWidget extends StatefulWidget {
-  @override
-  _MyCustomWidgetState createState() => new _MyCustomWidgetState();
-}
-
-class _MyCustomWidgetState extends State<MyCustomWidget> {
-  double _page = 10;
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    PageController pageController;
-    pageController = PageController(initialPage: 10);
-    pageController.addListener(
-      () {
-        setState(
-          () {
-            _page = pageController.page;
-          },
-        );
-      },
-    );
-
-    return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            SizedBox(
-              height: width,
-              width: width * .95,
-              child: LayoutBuilder(
-                builder: (context, boxConstraints) {
-                  List<Widget> cards = new List();
-
-                  for (int i = 0; i <= 11; i++) {
-                    double currentPageValue = i - _page;
-                    bool pageLocation = currentPageValue > 0;
-
-                    double start = 20 +
-                        max(
-                            (boxConstraints.maxWidth - width * .75) -
-                                ((boxConstraints.maxWidth - width * .75) / 2) *
-                                    -currentPageValue *
-                                    (pageLocation ? 9 : 1),
-                            0.0);
-
-                    var customizableCard = Positioned.directional(
-                      top: 20 + 30 * max(-currentPageValue, 0.0),
-                      bottom: 20 + 30 * max(-currentPageValue, 0.0),
-                      start: start,
-                      textDirection: TextDirection.ltr,
-                      child: Container(
-                          height: width * .67,
-                          width: width * .67,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: Colors.teal,
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black.withOpacity(.15),
-                                    blurRadius: 10)
-                              ])),
-                    );
-                    cards.add(customizableCard);
-                  }
-                  return Stack(children: cards);
-                },
-              ),
-            ),
-            Positioned.fill(
-              child: PageView.builder(
-                physics: BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics()),
-                itemCount: 11,
-                controller: pageController,
-                itemBuilder: (context, index) {
-                  return SizedBox();
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
       
